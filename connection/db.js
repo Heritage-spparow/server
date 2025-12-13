58bb9b66bbbf5ceeade5bfa633862a1755ebc273
Ayush Anand <anandayush865@gmail.com>
2025-12-13 23:26:33 +0530

    db fixes

--- connection/db.js
+++ connection/db.js
@@ -1,13 +1,32 @@
 const mongoose = require('mongoose');
 
+// The connection variable is kept outside the function scope.
+// In a serverless environment, this variable will persist across
+// warm lambda invocations.
+let cachedConnection = null;
+
 async function connect(uri) {
   if (!uri) {
     throw new Error('MONGO_URI is not set');
   }
-  if (mongoose.connection.readyState === 1) { 
+
+  if (mongoose.connection.readyState === 1) {
+    console.log('Using existing MongoDB connection.');
     return mongoose.connection;
   }
-  await mongoose.connect(uri);
+
+  if (mongoose.connection.readyState === 2 && cachedConnection) {
+     console.log('Waiting for connection in progress...');
+ 
+     await cachedConnection; 
+     return mongoose.connection;
+  }
+
+  // New connection attempt
+  console.log('Establishing new MongoDB connection...');
+  cachedConnection = mongoose.connect(uri);
+  await cachedConnection; 
+  
   return mongoose.connection;
 }
 
