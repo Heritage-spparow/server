65ce96471774ea83dec45380ee0f8261cb062057
Ayush Anand <anandayush865@gmail.com>
2026-01-07 04:02:11 +0530

    ssl fixed

--- utils/mailer.js
+++ utils/mailer.js
@@ -6,14 +6,24 @@ function getMailer() {
   if (!transporter) {
     transporter = nodemailer.createTransport({
       host: "smtp.titan.email",
-      port: 465,
-      secure: true,
+      port:465,          // 🔴 IMPORTANT
+      secure: true,       // 🔴 IMPORTANT
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD,
+        user: process.env.EMAIL_USER,      // support@heritagesparrow.com
+        pass: process.env.EMAIL_PASSWORD,  // NEW RESET PASSWORD
       },
     });
+
+    console.log(
+      "📨 Mailer initialized:",
+      process.env.EMAIL_HOST,
+      process.env.EMAIL_USER,
+      process.env.EMAIL_PASSWORD,
+      process.env.EMAIL_FROM
+
+    );
   }
+
   return transporter;
 }
 
